d338e2c657e0b7e1f458a574351b7aeaa8f89ce1
Im Jonghan <withyou@empal.com>
2024-11-22 15:16:30 +0900

    add options

--- manager/urls.py
+++ manager/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index_view, name='manager_home'),
     path('menu_list_url/', views.menu_list, name="menu_list_page"),
     path('menu_add_url/',views.menu_add, name="menu_add_page"), # 메뉴 추가 페이지
-    path('add_menu_data/', views.add_menu_data, name='add_menu_data')
+    path('add_menu_data/', views.add_menu_data, name='add_menu_data'),
+    # 메뉴 디테일 페이지 추가
+    path('detail/<int:menu_id>/', views.menu_detail, name="menu_detail"),
+    path('add_option/<int:menu_id>/', views.add_option, name='add_option'),
+    path('add_option_data/', views.add_option_data, name='add_option_data')
 ]
--- manager/views.py
+++ manager/views.py
@@ -16,16 +16,37 @@ def menu_add(request):
     return render(request, 'manager/menu_add_page.html')
 
 def add_menu_data(request):
-    menu_name_from_form = request.POST['menu_name']
-    menu_price_from_form = request.POST['menu_price']
-    # if menu_name_from_form: & menu_price_from_form>0:
+    menu_name_from_form = request.POST['menu_name'] # 받아온 메뉴이름 변수에 담기
+    if menu_name_from_form.strip()=='': # 제대로 된 값이 맞는지 검증해서 처리
+        context = {"message":"메뉴 이름을 넣으셔야 됩니다."}
+        return render(request, 'manager/menu_add_page.html', context)
+    menu_price_from_form = request.POST['menu_price'] # 받아온 메뉴가격 변수에 담기
+    # Menu 테이블에 가져온 값 저장하기
     Menu.objects.create(menu_name=menu_name_from_form, menu_price=menu_price_from_form)
-    print(f"메뉴이름 : {menu_name_from_form}, 가격 : {menu_price_from_form} 이 추가되었습니다. ")
+    print(f"메뉴이름 : {menu_name_from_form}, 가격 : {menu_price_from_form} 이 추가되었습니다. ") # log 추가
     
-    return render(request, 'manager/menu_add_page.html')
+    return render(request, 'manager/menu_add_page.html') # 이후 단계 페이지 설정
 
+   
+def menu_detail(request, menu_id):
+    # print("menu_id : ",menu_id)
+    menu = Menu.objects.get(pk=menu_id)
+    context = {
+       "menu_to_page" : menu
+    }
+    return render(request, 'manager/menu_detail.html', context)
 
+def add_option(request, menu_id):
+    menu = Menu.objects.get(pk=menu_id)
+    context = {"menu":menu}
+    return render(request, 'manager/add_option.html', context)
 
-
-
-
+def add_option_data(request):
+    menu_id_from_form = request.POST['menu_id']
+    option_name_from_form = request.POST['option_name']
+    option_price_from_form = request.POST['option_price']
+    # DB Opion Table에 저장
+    menu = Menu.objects.get(pk=menu_id_from_form)
+    Option.objects.create(menu=menu, option_name=option_name_from_form, option_price=option_price_from_form)
+    context = {"menu":menu}
+    return render(request, 'manager/add_option.html', context) # option 추가 화면 보여주기
